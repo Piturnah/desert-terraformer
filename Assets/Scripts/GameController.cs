7640e108d1c230d69c840e29c25c0ad3b034d0cc
Peter <peterhebden6@gmail.com>
2019-10-05 19:27:37 +0100

    gameplay setup

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public static float water;
+    public static float waterCap = 5000;
+
+    private void Awake()
+    {
+        Sprite[] mySprites = Resources.LoadAll<Sprite>("");
+    }
+}
--- Assets/Scripts/TerrainManager.cs
+++ Assets/Scripts/TerrainManager.cs
@@ -33,6 +33,18 @@ public class TerrainManager : MonoBehaviour
 
         GenerateTerrain();
     }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameController.water = Random.Range(1, 5000);
+            for (int i = 0; i < 91; i++)
+            {
+                UpdateTerrain(i * 2);
+            }
+            Debug.Log("Terrain Updated");
+        }
+    }
 
     void GenerateTerrain()
     {
@@ -61,6 +73,56 @@ public class TerrainManager : MonoBehaviour
 
                     newTile.transform.parent = transform;
                 }
+            }
+        }
+    }
+
+    void UpdateTerrain(int y)
+    {
+        for (int x = 0; x < mapSize.x; x++)
+        {
+            Sprite[] spritePool;
+            float perlinValue = Mathf.PerlinNoise((float)x / mapSize.x * perlinScale + perlinOffsetX, (float)y / mapSize.y * perlinScale + perlinOffsetY);
+
+            bool spawningRock = perlinValue <= rockRatio;
+            if (spawningRock)
+            {
+                if (perlinValue >= 1 - (GameController.water / GameController.waterCap))
+                {
+                    spritePool = rockTiles;
+                }
+                else
+                {
+                    spritePool = drockTiles;
+                }
+            }
+            else
+            {
+                if (perlinValue >= 1 - (GameController.water / GameController.waterCap))
+                {
+                    if (perlinValue >= 1 - (GameController.water / GameController.waterCap / 2))
+                    {
+                        spritePool = waterTiles;
+                    }
+                    else
+                    {
+                        spritePool = grassTiles;
+                    }
+                }
+                else
+                {
+                    spritePool = sandTiles;
+                }
+            }
+            int tileIndex = Random.Range(0, spritePool.Length - 1);
+
+            for (int i = 0; i < 2; i++)
+            {
+                GameObject newTile = Instantiate(tileObj, new Vector2(x * 2, y + i), Quaternion.identity);
+                SpriteRenderer tileSprite = newTile.GetComponent<SpriteRenderer>();
+                tileSprite.sprite = spritePool[(tileIndex + i) % spritePool.Length];
+
+                newTile.transform.parent = transform;
             }
         }
     }
